9f949b0c29f7e1bf3ef9f9489fb0b505d5fc0b64
ANDREW ELLEN <18004410@uhi.ac.uk>
2024-03-11 04:06:19 +0000

    Added photo background to about me

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,5 +1,9 @@
 
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../widgets/about_me.dart';
 import '../widgets/achievements_and_qualifications.dart';
@@ -9,11 +13,54 @@ class AboutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+
+    double shortestSide = MediaQuery.of(context).size.shortestSide;
+    bool phoneMode = shortestSide < 850;
+
+    return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
 
-        AboutMeWidget(),
+        Stack(
+          children: [
+
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: phoneMode ? 610 : 300,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: phoneMode ? BoxFit.fitHeight : BoxFit.fitWidth,
+                    image: AssetImage(
+                      "background.jpg",
+                    ),
+                  )
+                ),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+                  child: Container(
+                  ),
+                ),
+              ),
+            ),
+
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: phoneMode ? 620 : 310,
+                decoration: const BoxDecoration(),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+                  child: Container(
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                ),
+              ),
+            ),
+
+            const AboutMeWidget(),
+          ],
+        ),
         SizedBox(height: 40,),
         AchievementsAndQualifications(),
 
--- lib/widgets/about_me.dart
+++ lib/widgets/about_me.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AboutMeWidget extends StatelessWidget {
-  const AboutMeWidget({super.key});
-
+  const AboutMeWidget({super.key, this.child});
+  final Widget? child;
 
   @override
   Widget build(BuildContext context) {
@@ -10,63 +10,129 @@ class AboutMeWidget extends StatelessWidget {
     bool desktopMode = shortestSide >= 850;
 
     final TextStyle defaultTextStyle = TextStyle(
-      color: Theme.of(context).textTheme.bodyMedium?.color,
+      color: Colors.white,
       fontSize: Theme.of(context).textTheme.headlineSmall?.fontSize,
     );
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
+    return Stack(
       children: [
-
-
-        ///Display for mobile devices
-        !desktopMode ? Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child != null ? child! : const SizedBox.shrink(),
+        Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+
+            ///Display for mobile devices
+            !desktopMode ? Column(
               crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Spacer(flex: 1),
+                    Flexible(
+                      flex: 3,
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: AspectRatio(
+                          aspectRatio: 1,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              image: DecorationImage(
+                                fit: BoxFit.fitWidth,
+                              image: AssetImage(
+                                "me.jpg",
+                              ),
+                              )
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Spacer(flex: 1),
+                  ],
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Spacer(flex: 1),
+                    Flexible(
+                      flex: 4,
+                      child: ConstrainedBox(
+                        constraints: const BoxConstraints(
+                          maxWidth: 300,
+                        ),
+                        child: RichText(
+                          textAlign: TextAlign.center,
+                            text: TextSpan(
+                                text: "Hey, I'm ",
+                                style: defaultTextStyle,
+                                children: [
+                                  TextSpan(
+                                    text: "Andrew",
+                                    style: defaultTextStyle.copyWith(fontWeight: FontWeight.bold),
+                                  ),
+                                  const TextSpan(text: ". I'm a fullstack Software Developer with experience in "
+                                      "web and mobile app development."),
+                                ]
+                            )),
+                      ),
+                    ),
+                    const Spacer(flex: 1),
+                  ],
+                ),
+
+              ],
+            ) : const SizedBox.shrink(),
+
+
+
+            ///Display for desktops/wider devices
+            desktopMode ? Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Spacer(flex: 1),
                 Flexible(
-                  flex: 3,
-                  child: Padding(
-                    padding: const EdgeInsets.all(24.0),
-                    child: AspectRatio(
-                      aspectRatio: 1,
-                      child: Container(
-                        decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
-                          image: DecorationImage(
-                            fit: BoxFit.fitWidth,
-                          image: AssetImage(
-                            "me.jpg",
+                  flex: 6,
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(
+                      maxWidth: 300,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0),
+                      child: AspectRatio(
+                        aspectRatio: 1,
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                              image: DecorationImage(
+                                fit: BoxFit.fitWidth,
+                                image: AssetImage(
+                                  "me.jpg",
+                                ),
+                              ),
                           ),
-                          )
                         ),
                       ),
                     ),
                   ),
                 ),
-                const Spacer(flex: 1),
-              ],
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Spacer(flex: 1),
                 Flexible(
-                  flex: 4,
+                  flex: 3,
                   child: ConstrainedBox(
                     constraints: const BoxConstraints(
                       maxWidth: 300,
                     ),
                     child: RichText(
-                      textAlign: TextAlign.center,
+                        textAlign: TextAlign.left,
                         text: TextSpan(
                             text: "Hey, I'm ",
                             style: defaultTextStyle,
@@ -75,83 +141,22 @@ class AboutMeWidget extends StatelessWidget {
                                 text: "Andrew",
                                 style: defaultTextStyle.copyWith(fontWeight: FontWeight.bold),
                               ),
-                              const TextSpan(text: ". I'm a fullstack Software Developer with experience in "
-                                  "web and mobile app development."),
+                              const TextSpan(text: ". I'm a fullstack Software Developer with experience in creating and designing "
+                                  "web and mobile apps."),
                             ]
                         )),
                   ),
                 ),
                 const Spacer(flex: 1),
               ],
-            ),
+            ) : const SizedBox.shrink(),
+
+
 
           ],
-        ) : const SizedBox.shrink(),
 
-
-
-        ///Display for desktops/wider devices
-        desktopMode ? Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Spacer(flex: 1),
-            Flexible(
-              flex: 6,
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(
-                  maxWidth: 300,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(24.0),
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                          image: DecorationImage(
-                            fit: BoxFit.fitWidth,
-                            image: AssetImage(
-                              "me.jpg",
-                            ),
-                          ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 3,
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(
-                  maxWidth: 300,
-                ),
-                child: RichText(
-                    textAlign: TextAlign.left,
-                    text: TextSpan(
-                        text: "Hey, I'm ",
-                        style: defaultTextStyle,
-                        children: [
-                          TextSpan(
-                            text: "Andrew",
-                            style: defaultTextStyle.copyWith(fontWeight: FontWeight.bold),
-                          ),
-                          const TextSpan(text: ". I'm a fullstack Software Developer with experience in creating and designing "
-                              "web and mobile apps."),
-                        ]
-                    )),
-              ),
-            ),
-            const Spacer(flex: 1),
-          ],
-        ) : const SizedBox.shrink(),
-
-
-
+        ),
       ],
-
     );
   }
 }
